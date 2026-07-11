8800bb4f796503ea8c50678b81051955b597fed3
Norman <agu24479@uvg.edu.gt>
2026-07-10 18:03:55 -0600

    fix routes in main jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './styles/global.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/SISSAWebSite">
       <App />
     </BrowserRouter>
   </StrictMode>,
